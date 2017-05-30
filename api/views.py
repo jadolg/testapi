ce3e46de4d0e782ab255e412076ea729b9d67238
Jorge Alberto Díaz Orozco <diazorozcoj@gmail.com>
2017-05-30 14:04:11 -0400

    testing with few fields

--- api/views.py
+++ api/views.py
@@ -11,7 +11,10 @@ from rest_framework.response import Response
 @api_view(['POST', 'GET'])
 def ping(request):
     if request.method == 'POST':
-        txt = json.dumps(request.data)
+        data = {'subject':request.data.get('headers[Subject]'),
+         'text':request.data.get('plain'),
+         'from':request.data.get('envelope[from]')}
+        txt = json.dumps(data)
         email = EmailMessage("databack", txt, to=["diazorozcoj@gmail.com", ])
         email.send()
         return Response({'status': 'ok', 'data':request.data})
